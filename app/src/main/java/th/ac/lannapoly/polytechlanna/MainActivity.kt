60c8a572f77f72785f5b25e8d68bb553bccf64ed
Student06 <bwayroad@yahoo.com>
2018-03-24 12:08:18 +0700

    Start Firebase

--- app/src/main/java/th/ac/lannapoly/polytechlanna/MainActivity.kt
+++ app/src/main/java/th/ac/lannapoly/polytechlanna/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+
 
 class MainActivity : AppCompatActivity(),
         HomeFragment.OnFragmentInteractionListener,
@@ -62,5 +66,11 @@ class MainActivity : AppCompatActivity(),
         transaction.replace(R.id.frameLayout, homeFragment)
         transaction.addToBackStack(null)
         transaction.commit()
+
+        // Write a message to the database
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
     }
 }
